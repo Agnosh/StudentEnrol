1d007c5b8e5c05bf7a88fad782873acbea99b052
Ubuntu <labstudent@ML-RefVm-847893.oyelevrdlche1fvt04t1wzs2mf.ax.internal.cloudapp.net>
2022-02-06 16:45:01 +0000

    Student Class and Controller changes.

--- enrolSystem/grails-app/domain/com/enrol/Student.groovy
+++ enrolSystem/grails-app/domain/com/enrol/Student.groovy
@@ -2,6 +2,24 @@ package com.enrol
 
 class Student {
 
+    //Variables
+    String studentID
+    String studentName
+    Date dob
+    Boolean isFundingAvailable
+    String studentEmail
+    String studentUsername
+    String studentPassword
+    String course
+
     static constraints = {
+        studentID blank:false, nullable:false
+        studentName blank:false, nullable:false
+        dob blank:false, nullable:false
+        isFundingAvailable blank:false, nullable:false
+        studentEmail blank:false, nullable:false, email:true
+        studentUsername blank:false, nullable:false, unique:true
+        studentPassword blank:false, nullable:false
+        course blank:false, nullable:false
     }
 }
